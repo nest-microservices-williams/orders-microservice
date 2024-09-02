54d1809489cddebea8c741b7b444351b8c3b3044
Williams Mata <williams.rm99@gmail.com>
2024-09-02 12:11:37 -0400

    Connect prisma with database

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, PrismaService],
 })
 export class OrdersModule {}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class OrdersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }

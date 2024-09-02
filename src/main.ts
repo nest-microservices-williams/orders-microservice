0fa613d43f3db89fcd6b87df31d3cac59f071e5e
Williams Mata <williams.rm99@gmail.com>
2024-09-02 11:05:22 -0400

    Transform app to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
 
 async function bootstrap() {
   const logger = new Logger('Orders bootstrap');
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+
+  await app.listen();
 
   logger.log(`Application listening on port ${envs.port}`);
 }
